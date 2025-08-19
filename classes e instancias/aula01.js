6af8eddfda1965419b22a8ed31dff34ee20acd6a
vinicusd7 <viniciusdm967@gmail.com>
2025-08-19 12:29:04 -0300

    atualizando

---
+++ classes e instancias/aula01.js
@@ -0,0 +1,25 @@
+
+class pessoa { //classe uma definição do que deveria ser 
+    nome;
+    idade;
+    
+    descrever() {
+        console.log(`meu nome é  ${this.nome} e minha idade é ${this.idade}`);
+    }
+}
+
+
+// a instancia e uma ocorrencia de uma pessoa neste caso
+const vitor = new pessoa();
+
+console.log('vitor');
+vitor.nome = 'Vitor';
+vitor.idade = 25;
+
+console.log('marcelo');
+const marcelo = new pessoa();
+marcelo.nome = 'Marcelo';
+marcelo.idade = 30;
+
+console.log(vitor);
+console.log(marcelo);   
