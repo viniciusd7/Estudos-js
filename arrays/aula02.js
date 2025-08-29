863b3008a1cf76e7971533f44887e1fda90832f3
vinicusd7 <viniciusdm967@gmail.com>
2025-08-29 10:16:49 -0300

    atualizando

--- arrays/aula02.js
+++ arrays/aula02.js
@@ -1,6 +1,6 @@
-//criando uma lista e inserindo dados a ela notas de um aluno 
+//criando uma lista e inserindo dados a ela, notas de um aluno.
 //length mostra o tamanho da lista 
-
+//calculando a media de notas 
 const notas = [];
 
 notas.push(5);
@@ -9,6 +9,13 @@ notas.push(2);
 notas.push(8);
 notas.push(5);
 
-const soma = notas [0] + notas[1] + notas[2] + notas[3] + notas[4];
+let soma = 0;
 
-console.log(soma/5);
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota;
+    
+}
+
+const media = soma / notas.length;
+console.log(media);
